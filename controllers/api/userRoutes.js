cce0e59933bee69f5ee81a6863031687f32a2f34
LittleMagician <bthaoxaochay@gmail.com>
2021-03-22 14:03:14 -0500

    Adding templates and linking front-end JS.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,2 +1,11 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
