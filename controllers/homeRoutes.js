76c4869526e61c80a4a55daaa142140a6e6bd536
LittleMagician <bthaoxaochay@gmail.com>
2021-03-22 13:18:02 -0500

    Code to render blogposts to homepage.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
-const withAuth = require('../utils/auth');
+const { Blogposts, User } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try { // Getting all blogposts in the database
+        const blogpostData = await Blogposts.findAll({
+            include: [
+                {
+                    model: Blogposts, // Model
+                    attributes: ['name', 'description', 'date_created'] // Info needed from model
+                }
+            ]
+        })
+
+        const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+
+        // Rendering onto the homepage
+        res.render('homepage', {
+            blogposts
+        });
+    } 
+    catch (err) { // Catching any errors
+        res.status(500).json(err);
+    }
+})
